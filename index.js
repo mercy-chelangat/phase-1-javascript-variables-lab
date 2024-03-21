0731350454609c8d0fda8d286bd2ad7fcab423c7
mercy chelangat <chelangatmercy194@gmail.com>
2024-03-21 14:23:08 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+// Exporting variables for testing purposes
+module.exports = { companyName, mostProfitableNeighborhood, companyCeo };
+
